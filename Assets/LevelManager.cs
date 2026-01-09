0964c2978d09c99f433d01828a3e6a86ad5b33c1
Neptunus <neptunus.hallje@gmail.com>
2026-01-09 09:52:44 +0100

    Lade till en knapp och script för att byta scen

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public string sceneName;
+  
+
+    public void changeScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
